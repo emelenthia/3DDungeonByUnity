e99ab46d0a4547b4fd54a6ddb1dd294c786c1760
amber <p0amberayuayu@gmail.com>
2020-12-31 00:36:36 +0900

    init commit

---
+++ Assets/Script/CameraMover.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class CameraMover : MonoBehaviour
+{
+    // WASD：前後左右の移動
+    // QE：上昇・降下
+    // 右ドラッグ：カメラの回転
+    // 左ドラッグ：前後左右の移動
+    // スペース：カメラ操作の有効・無効の切り替え
+    // P：回転を実行時の状態に初期化する
+
+    //カメラの移動量
+    [SerializeField, Range(0.1f, 10.0f)]
+    private float _positionStep = 2.0f;
+
+    //マウス感度
+    [SerializeField, Range(30.0f, 150.0f)]
+    private float _mouseSensitive = 90.0f;
+
+    //カメラ操作の有効無効
+    private bool _cameraMoveActive = true;
+    //カメラのtransform  
+    private Transform _camTransform;
+    //マウスの始点 
+    private Vector3 _startMousePos;
+    //カメラ回転の始点情報
+    private Vector3 _presentCamRotation;
+    private Vector3 _presentCamPos;
+    //初期状態 Rotation
+    private Quaternion _initialCamRotation;
+    //UIメッセージの表示
+    private bool _uiMessageActiv;
+    private static Timer myTimer;
+    private static bool isMoving = false; 
+    private static int moveTime = 0;
+    private static Vector3 tempPos;
+    private const int MOVE_FRAME = 30;
+    private static float tempTransformX = 0.0f;
+    private static float tempTransformY = 0.0f;
+    private Direction direction = Direction.Forward;
+
+    enum Direction {
+        Forward = 0,
+        Right = 1,
+        Back = 2,
+        Left = 3
+    }
+
+    void Start ()
+    {
+        _camTransform = this.gameObject.transform;
+
+        //初期回転の保存
+        _initialCamRotation = this.gameObject.transform.rotation;
+    }
+
+    void Update () {
+
+        CamControlIsActive(); //カメラ操作の有効無効
+
+        if (_cameraMoveActive)
+        {
+            ResetCameraRotation(); //回転角度のみリセット
+            CameraRotationMouseControl(); //カメラの回転 マウス
+            CameraSlideMouseControl(); //カメラの縦横移動 マウス
+            CameraPositionKeyControl(); //カメラのローカル移動 キー
+        }
+    }
+
+    //カメラ操作の有効無効
+    public void CamControlIsActive()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _cameraMoveActive = !_cameraMoveActive;
+
+            if (_uiMessageActiv == false)
+            {
+                StartCoroutine(DisplayUiMessage());
+            }            
+            Debug.Log("CamControl : " + _cameraMoveActive);
+        }
+    }
+
+    //回転を初期状態にする
+    private void ResetCameraRotation()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            this.gameObject.transform.rotation = _initialCamRotation;
+            Debug.Log("Cam Rotate : " + _initialCamRotation.ToString());    
+        }
+    }
+
+    //カメラの回転 マウス
+    private void CameraRotationMouseControl()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _startMousePos = Input.mousePosition;
+            _presentCamRotation.x = _camTransform.transform.eulerAngles.x;
+            _presentCamRotation.y = _camTransform.transform.eulerAngles.y;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //(移動開始座標 - マウスの現在座標) / 解像度 で正規化
+            float x = (_startMousePos.x - Input.mousePosition.x) / Screen.width;
+            float y = (_startMousePos.y - Input.mousePosition.y) / Screen.height;
+
+            //回転開始角度 ＋ マウスの変化量 * マウス感度
+            float eulerX = _presentCamRotation.x + y * _mouseSensitive;
+            float eulerY = _presentCamRotation.y + x * _mouseSensitive;
+
+            _camTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+        }
+    }
+
+    //カメラの移動 マウス
+    private void CameraSlideMouseControl()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _startMousePos = Input.mousePosition;
+            _presentCamPos = _camTransform.position;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            //(移動開始座標 - マウスの現在座標) / 解像度 で正規化
+            float x = (_startMousePos.x - Input.mousePosition.x) / Screen.width;
+            float y = (_startMousePos.y - Input.mousePosition.y) / Screen.height;
+
+            x = x * _positionStep;
+            y = y * _positionStep;
+
+            Vector3 velocity = _camTransform.rotation * new Vector3(x, y, 0);
+            velocity = velocity + _presentCamPos;
+            _camTransform.position = velocity;
+        }
+    }
+
+    //カメラのローカル移動 キー
+    private void CameraPositionKeyControl()
+    {
+        if (isMoving) {
+            return;
+        }
+
+        Vector3 campos = _camTransform.position;
+        tempPos = campos;
+        tempTransformX = _camTransform.transform.eulerAngles.x;
+        tempTransformY = _camTransform.transform.eulerAngles.y;
+
+        if (Input.GetKey(KeyCode.D)) { 
+            isMoving = true;
+            InvokeRepeating("rotateRight", 0.0f, (1.0f / MOVE_FRAME));
+        }
+        if (Input.GetKey(KeyCode.A)) { 
+            isMoving = true;
+            InvokeRepeating("rotateLeft", 0.0f, (1.0f / MOVE_FRAME));
+        }
+        
+        if (Input.GetKey(KeyCode.W)) { 
+            isMoving = true;
+            switch (direction) {
+                case Direction.Forward:
+                    InvokeRepeating("moveForward", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Right:
+                    InvokeRepeating("moveRight", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Back:
+                    InvokeRepeating("moveBack", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Left:
+                    InvokeRepeating("moveLeft", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (Input.GetKey(KeyCode.S)) { 
+            isMoving = true;
+            switch (direction) {
+                case Direction.Forward:
+                    InvokeRepeating("moveBack", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Right:
+                    InvokeRepeating("moveLeft", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Back:
+                    InvokeRepeating("moveForward", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                case Direction.Left:
+                    InvokeRepeating("moveright", 0.0f, (1.0f / MOVE_FRAME));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.E)) { campos += _camTransform.up * Time.deltaTime * _positionStep; }
+        if (Input.GetKey(KeyCode.Q)) { campos -= _camTransform.up * Time.deltaTime * _positionStep; }
+
+        _camTransform.position = campos;
+    }
+
+    private void moveForward() {
+        Vector3 campos = _camTransform.position;
+        campos = new Vector3(campos.x + (1.0f / MOVE_FRAME), campos.y, campos.z);
+        moveTime++;
+        Debug.Log("ee");
+        if (moveTime > MOVE_FRAME) {
+            Debug.Log("ff");
+            campos = new Vector3(tempPos.x + 1, campos.y, campos.z);
+            isMoving = false;
+            moveTime = 0;
+            CancelInvoke();
+        }
+        _camTransform.position = campos;
+    }
+
+    private void moveRight() {
+        Vector3 campos = _camTransform.position;
+        campos = new Vector3(campos.x, campos.y, campos.z - (1.0f / MOVE_FRAME));
+        moveTime++;
+        Debug.Log("ee");
+        if (moveTime > MOVE_FRAME) {
+            Debug.Log("ff");
+            campos = new Vector3(tempPos.x, campos.y, campos.z - 1);
+            isMoving = false;
+            moveTime = 0;
+            CancelInvoke();
+        }
+        _camTransform.position = campos;
+    }
+    private void moveLeft() {
+        Vector3 campos = _camTransform.position;
+        campos = new Vector3(campos.x, campos.y, campos.z + (1.0f / MOVE_FRAME));
+        moveTime++;
+        Debug.Log("ee");
+        if (moveTime > MOVE_FRAME) {
+            Debug.Log("ff");
+            campos = new Vector3(tempPos.x, campos.y, campos.z + 1);
+            isMoving = false;
+            moveTime = 0;
+            CancelInvoke();
+        }
+        _camTransform.position = campos;
+    }
+    private void moveBack() {
+        Vector3 campos = _camTransform.position;
+        campos = new Vector3(campos.x - (1.0f / MOVE_FRAME), campos.y, campos.z);
+        moveTime++;
+        Debug.Log("ee");
+        if (moveTime > MOVE_FRAME) {
+            Debug.Log("ff");
+            campos = new Vector3(tempPos.x - 1, campos.y, campos.z);
+            isMoving = false;
+            moveTime = 0;
+            CancelInvoke();
+        }
+        _camTransform.position = campos;
+    }
+
+    private void rotateRight() {
+        float eulerX = _camTransform.transform.eulerAngles.x;
+        float eulerY = _camTransform.transform.eulerAngles.y + (90.0f / MOVE_FRAME);
+        Debug.Log(tempTransformY);
+        if (moveTime < MOVE_FRAME) {
+            moveTime++;
+
+            switch (direction)
+            {
+                case Direction.Forward:
+                    _camTransform.position = new Vector3(_camTransform.position.x + (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z + (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Right:
+                    _camTransform.position = new Vector3(_camTransform.position.x + (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z - (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Back:
+                    _camTransform.position = new Vector3(_camTransform.position.x - (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z - (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Left:
+                    _camTransform.position = new Vector3(_camTransform.position.x - (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z + (0.5f / MOVE_FRAME));
+                    break;
+                default:
+                    break;
+            }
+            _camTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+            return;
+        }
+        eulerY = tempTransformY + 90;
+            switch (direction)
+            {
+                case Direction.Forward:
+                    direction = Direction.Right;
+                    _camTransform.position = new Vector3(tempPos.x + 0.5f, tempPos.y, tempPos.z + 0.5f);
+                    break;
+                case Direction.Right:
+                    direction = Direction.Back;
+                    _camTransform.position = new Vector3(tempPos.x + 0.5f, tempPos.y, tempPos.z - 0.5f);
+                    break;
+                case Direction.Back:
+                    direction = Direction.Left;
+                    _camTransform.position = new Vector3(tempPos.x - 0.5f, tempPos.y, tempPos.z - 0.5f);
+                    break;
+                case Direction.Left:
+                    direction = Direction.Forward;
+                    _camTransform.position = new Vector3(tempPos.x - 0.5f, tempPos.y, tempPos.z + 0.5f);
+                    break;
+                default:
+                    break;
+            }
+        _camTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+
+        isMoving = false;
+        moveTime = 0;
+        CancelInvoke();
+    }
+    private void rotateLeft() {
+        float eulerX = _camTransform.transform.eulerAngles.x;
+        float eulerY = _camTransform.transform.eulerAngles.y - (90.0f / MOVE_FRAME);
+        if (moveTime < MOVE_FRAME) {
+            moveTime++;
+
+            switch (direction)
+            {
+                case Direction.Forward:
+                    _camTransform.position = new Vector3(_camTransform.position.x + (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z - (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Right:
+                    _camTransform.position = new Vector3(_camTransform.position.x - (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z - (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Back:
+                    _camTransform.position = new Vector3(_camTransform.position.x - (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z + (0.5f / MOVE_FRAME));
+                    break;
+                case Direction.Left:
+                    _camTransform.position = new Vector3(_camTransform.position.x + (0.5f / MOVE_FRAME), tempPos.y, _camTransform.position.z + (0.5f / MOVE_FRAME));
+                    break;
+                default:
+                    break;
+            }
+            _camTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+            return;
+        }
+        Debug.Log(tempTransformY);
+        eulerY = tempTransformY - 90;
+            switch (direction)
+            {
+                case Direction.Forward:
+                    direction = Direction.Left;
+                    _camTransform.position = new Vector3(tempPos.x + 0.5f, tempPos.y, tempPos.z - 0.5f);
+                    break;
+                case Direction.Right:
+                    direction = Direction.Forward;
+                    _camTransform.position = new Vector3(tempPos.x - 0.5f, tempPos.y, tempPos.z - 0.5f);
+                    break;
+                case Direction.Back:
+                    direction = Direction.Right;
+                    _camTransform.position = new Vector3(tempPos.x - 0.5f, tempPos.y, tempPos.z + 0.5f);
+                    break;
+                case Direction.Left:
+                    direction = Direction.Back;
+                    _camTransform.position = new Vector3(tempPos.x + 0.5f, tempPos.y, tempPos.z + 0.5f);
+                    break;
+                default:
+                    break;
+            }
+        _camTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+
+        isMoving = false;
+        moveTime = 0;
+        CancelInvoke();
+    }
+
+
+    //UIメッセージの表示
+    private IEnumerator DisplayUiMessage()
+    {
+        _uiMessageActiv = true;
+        float time = 0;
+        while (time < 2)
+        {
+            time = time + Time.deltaTime;
+            yield return null;
+        }
+        _uiMessageActiv = false;
+    }
+
+    void OnGUI()
+    {
+        if (_uiMessageActiv == false) { return; }
+        GUI.color = Color.black;
+        if (_cameraMoveActive == true)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height - 30, 100, 20), "カメラ操作 有効");
+        }
+
+        if (_cameraMoveActive == false)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height - 30, 100, 20), "カメラ操作 無効");
+        }
+    }
+
+}
---
+++ Assets/Script/MapGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+　　  //こっちは中身のマップ用のプレハブ
+	public GameObject upWallPrefab;
+	public GameObject rightWallPrefab;
+	public GameObject downWallPrefab;
+	public GameObject leftWallPrefab;
+	public GameObject floorPrefab;
+
+	private int default_x_max = 19;
+	private int default_z_max = 19;
+    private int mapX = 10;
+    private int mapy = 10;
+
+    /*
+    【マップについて】
+        マップは以下の情報を持つ、マス目情報の集合体とする
+            ・4方に壁が存在するかどうか
+        4方に壁が存在するかは、各bitが立っているかどうかで判別する
+            ・1…上側に壁が存在する
+            ・2…右側に壁が存在する
+            ・4…下側に壁が存在する
+            ・8…左側に壁が存在する
+        現在居るマス目から、壁のある方向には進むことが出来ない
+        [2,0]というマップが存在する場合、1マス目から2マス目へ移動することは出来ないが、2マス目から1マス目へ移動することはできる(一方通行の再現)
+    */
+
+	// Use this for initialization
+	void Start () {
+		//これがマップの元になるデータ
+        int[][] map = new int[][] {
+            new int[] {9, 5, 5, 5, 5, 1, 1, 1, 1, 3},
+            new int[] {10, 9, 1, 1, 1, 0, 0, 0, 0, 2},
+            new int[] {10, 8, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {10, 8, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {10, 8, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {8, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {8, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {8, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {8, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+            new int[] {12, 4, 4, 4, 4, 4, 4, 4, 4, 6}
+        };
+		
+        Debug.Log("Hello, World");
+		
+//        Instantiate(upWallPrefab, new Vector3(1 + 0.5f, 0.5f, mapy - 1), Quaternion.identity);
+//        Instantiate(rightWallPrefab, new Vector3(1 + 1, 0.5f, mapy - 1.5f), Quaternion.identity);
+//        Instantiate(downWallPrefab, new Vector3(1 + 0.5f, 0.5f, mapy - 2), Quaternion.identity);
+//        Instantiate(leftWallPrefab, new Vector3(1 + 0, 0.5f, mapy - 1.5f), Quaternion.identity);
+//        Instantiate(floorPrefab, new Vector3(0.5f, 0, mapy - 0.5f), Quaternion.identity);
+//        Instantiate(floorPrefab, new Vector3(1.5f, 0, mapy - 1.5f), Quaternion.identity);
+		// 引数にこれを入れてマップ生成する
+		CreateMap(map);
+	}
+	
+	//マップを作るメソッド
+	void CreateMap(int[][] map)	{
+        const float height = 0.5f;
+        for (int y = 0; y < map.Length; y++) {
+            for (int x = 0; x < map[y].Length; x++) {
+                if ((map[y][x] & 1) == 1) {
+                	Instantiate(upWallPrefab, new Vector3(x + 0.5f, height, mapy - y), Quaternion.identity);
+                }
+                if ((map[y][x] & 2) == 2) {
+                	Instantiate(rightWallPrefab, new Vector3(x + 1, height, mapy - (y + 0.5f)), Quaternion.identity);
+                }
+                if ((map[y][x] & 4) == 4) {
+                	Instantiate(downWallPrefab, new Vector3(x + 0.5f, height, mapy - (y + 1)), Quaternion.identity);
+                }
+                if ((map[y][x] & 8) == 8) {
+                	Instantiate(leftWallPrefab, new Vector3(x + 0, height, mapy - (y + 0.5f)), Quaternion.identity);
+                }
+                //床
+                Instantiate(floorPrefab, new Vector3(x + 0.5f, 0, mapy - (y + 0.5f)), Quaternion.identity);
+                //天井
+                Instantiate(floorPrefab, new Vector3(x + 0.5f, 1, mapy - (y + 0.5f)), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
